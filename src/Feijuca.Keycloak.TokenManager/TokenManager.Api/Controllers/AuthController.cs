a6321c482529e03cf5528499d8eb830d6c75595b
Felipe Mattioli dos Santos <felipe.mattiolidossantos@msc.com>
2024-09-20 17:35:47 +0200

    FEAT: Changing endpoints to use restful pattern

---
+++ src/Feijuca.Keycloak.TokenManager/TokenManager.Api/Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TokenManager.Application.Commands.Users;
+using TokenManager.Application.Requests.User;
+using TokenManager.Application.Responses;
+using TokenManager.Common.Models;
+
+namespace TokenManager.Api.Controllers
+{
+    public class AuthController(IMediator mediator) : ControllerBase
+    {
+        private readonly IMediator _mediator = mediator;
+
+        /// <summary>
+        /// Authenticate a user and return a valid JWT token and details.
+        /// </summary>
+        /// <returns>A status code related to the operation.</returns>
+        [HttpPost]
+        [Route("auth/{tenant}/login", Name = nameof(Login))]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Login([FromRoute] string tenant, [FromBody] LoginUserRequest loginUserRequest, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(new LoginUserCommand(tenant, loginUserRequest), cancellationToken);
+
+            if (result.IsSuccess)
+            {
+                var response = Result<TokenDetailsResponse>.Success(result.Data);
+                return Ok(response.Data); // Retorna o token e os detalhes
+            }
+
+            var responseError = Result<TokenDetailsResponse>.Failure(result.Error);
+            return BadRequest(responseError);
+        }
+
+        /// <summary>
+        /// Return a valid JWT token and details about them refreshed.
+        /// </summary>
+        /// <returns>A status code related to the operation.</returns>
+        [HttpPut]
+        [Route("auth/{tenant}/tokens/refresh", Name = nameof(RefreshToken))]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Authorize]
+        public async Task<IActionResult> RefreshToken([FromRoute] string tenant, [FromBody] RefreshTokenRequest request, CancellationToken cancellationToken)
+        {
+            var result = await _mediator.Send(new RefreshTokenCommand(tenant, request.RefreshToken), cancellationToken);
+
+            if (result.IsSuccess)
+            {
+                return Ok(result.Data); // Retorna o token atualizado
+            }
+
+            return BadRequest(result.Error);
+        }
+    }
+}
--- src/Feijuca.Keycloak.TokenManager/TokenManager.Api/Controllers/UserController.cs
+++ src/Feijuca.Keycloak.TokenManager/TokenManager.Api/Controllers/UserController.cs
@@ -1,14 +1,10 @@
 ﻿using Feijuca.Keycloak.MultiTenancy.Attributes;
-
 using MediatR;
-
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-
 using TokenManager.Application.Commands.Users;
 using TokenManager.Application.Queries.Users;
 using TokenManager.Application.Requests.User;
-using TokenManager.Application.Responses;
 using TokenManager.Common.Models;
 
 namespace TokenManager.Api.Controllers
@@ -24,7 +20,7 @@ namespace TokenManager.Api.Controllers
         /// </summary>
         /// <returns>A status code related to the operation.</returns>
         [HttpGet]
-        [Route("getUsers/{tenant}", Name = nameof(GetUsers))]
+        [Route("users/{tenant}", Name = nameof(GetUsers))]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -48,20 +44,20 @@ namespace TokenManager.Api.Controllers
         /// </summary>
         /// <returns>A status code related to the operation.</returns>
         [HttpPost]
-        [Route("createUser/{tenant}", Name = nameof(Create))]
+        [Route("users/{tenant}", Name = nameof(CreateUser))]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [RequiredRole("Feijuca.ApiWriter")]
         [Authorize]
-        public async Task<IActionResult> Create([FromRoute] string tenant, [FromBody] AddUserRequest addUserRequest, CancellationToken cancellationToken)
+        public async Task<IActionResult> CreateUser([FromRoute] string tenant, [FromBody] AddUserRequest addUserRequest, CancellationToken cancellationToken)
         {
             var result = await _mediator.Send(new CreateUserCommand(tenant, addUserRequest), cancellationToken);
 
             if (result.IsSuccess)
             {
                 var response = Result<string>.Success("User created successfully");
-                return Created("/createUser", response.Data);
+                return Created($"/api/v1/users/{tenant}", response.Data);
             }
 
             var responseError = Result<string>.Failure(result.Error);
@@ -69,19 +65,19 @@ namespace TokenManager.Api.Controllers
         }
 
         /// <summary>
-        /// DeleteGroup an existing user on the Keycloak realm.
+        /// Delete an existing user on the Keycloak realm.
         /// </summary>
         /// <returns>A status code related to the operation.</returns>
         [HttpDelete]
-        [Route("deleteUser/{tenant}", Name = nameof(Delete))]
+        [Route("users/{tenant}/{userId}", Name = nameof(DeleteUser))]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [RequiredRole("Feijuca.ApiWriter")]
         [Authorize]
-        public async Task<IActionResult> Delete([FromRoute] string tenant, [FromBody] DeleteUserRequest deleteUserRequest, CancellationToken cancellationToken)
+        public async Task<IActionResult> DeleteUser([FromRoute] string tenant, [FromRoute] Guid userId, CancellationToken cancellationToken)
         {
-            var result = await _mediator.Send(new DeleteUserCommand(tenant, deleteUserRequest.UserId), cancellationToken);
+            var result = await _mediator.Send(new DeleteUserCommand(tenant, userId), cancellationToken);
 
             if (result.IsSuccess)
             {
@@ -89,54 +85,6 @@ namespace TokenManager.Api.Controllers
             }
 
             var responseError = Result<string>.Failure(result.Error);
-            return BadRequest(responseError);
-        }
-
-        /// <summary>
-        /// Return a valid JWT token and details about them.
-        /// </summary>
-        /// <returns>A status code related to the operation.</returns>
-        [HttpPost]
-        [Route("login/{tenant}", Name = nameof(Login))]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> Login([FromRoute] string tenant, [FromBody] LoginUserRequest loginUserRequest, CancellationToken cancellationToken)
-        {
-            var result = await _mediator.Send(new LoginUserCommand(tenant, loginUserRequest), cancellationToken);
-
-            if (result.IsSuccess)
-            {
-                var response = Result<TokenDetailsResponse>.Success(result.Data);
-                return Ok(response.Data);
-            }
-
-            var responseError = Result<TokenDetailsResponse>.Failure(result.Error);
-            return BadRequest(responseError);
-        }
-
-        /// <summary>
-        /// Return a valid JWT token and details about them refreshed.
-        /// </summary>
-        /// <returns>A status code related to the operation.</returns>
-        [HttpPost]
-        [Route("refreshToken/{tenant}", Name = nameof(RefreshToken))]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        [RequiredRole("Feijuca.ApiWriter")]
-        [Authorize]
-        public async Task<IActionResult> RefreshToken([FromRoute] string tenant, [FromBody] RefreshTokenRequest request, CancellationToken cancellationToken)
-        {
-            var result = await _mediator.Send(new RefreshTokenCommand(tenant, request.RefreshToken), cancellationToken);
-
-            if (result.IsSuccess)
-            {
-                var response = Result<TokenDetailsResponse>.Success(result.Data);
-                return Ok(response);
-            }
-
-            var responseError = Result<TokenDetailsResponse>.Failure(result.Error);
             return BadRequest(responseError);
         }
     }
