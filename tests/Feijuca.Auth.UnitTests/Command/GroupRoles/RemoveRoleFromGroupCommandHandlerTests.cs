53b337ab9944e32207bf83518f929d8f6fbc13eb
Weslley Souza <weslleyjunio3032@gmail.com>
2024-10-29 13:30:57 -0300

    test: Finished tests

--- tests/Feijuca.Auth.UnitTests/Command/GroupRoles/RemoveRoleFromGroupCommandHandlerTests.cs
+++ tests/Feijuca.Auth.UnitTests/Command/GroupRoles/RemoveRoleFromGroupCommandHandlerTests.cs
@@ -22,56 +22,56 @@ namespace Feijuca.Auth.Api.UnitTests.Command.GroupRoles
             _handler = new RemoveRoleFromGroupCommandHandler(_groupRepositoryMock.Object, _groupRolesRepositoryMock.Object, _roleRepositoryMock.Object);
         }
 
-        //[Fact]
-        //public async Task Given_ValidGroupAndRole_When_RemoveRoleFromGroupIsCalled_Then_ShouldRemoveRoleSuccessfully()
-        //{
-        //    // Arrange
-        //    var command = _fixture.Create<RemoveRoleFromGroupCommand>();
-        //    var cancellationToken = _fixture.Create<CancellationToken>();
+        [Fact]
+        public async Task Given_ValidGroupAndRole_When_RemoveRoleFromGroupIsCalled_Then_ShouldRemoveRoleSuccessfully()
+        {
+            // Arrange
+            var command = _fixture.Create<RemoveRoleFromGroupCommand>();
+            var cancellationToken = _fixture.Create<CancellationToken>();
 
-        //    var groups = _fixture.Build<Group>()
-        //             .With(g => g.Id, command.GroupId)
-        //             .CreateMany(1); // Erro esta acontecendo aqui a principio.
+            var groups = _fixture.Build<Group>()
+                     .With(g => g.Id, command.GroupId)
+                     .CreateMany(1);
 
-        //    var roles = _fixture.Build<Role>()
-        //            .With(r => r.Id, command.RemoveRoleFromGroupRequest.RoleId)
-        //            .With(r => r.Name, "ValidRoleName")
-        //            .CreateMany(1);
+            var roles = _fixture.Build<Role>()
+                    .With(r => r.Id, command.RemoveRoleFromGroupRequest.RoleId)
+                    .With(r => r.Name, "ValidRoleName")
+                    .CreateMany(1);
 
-        //    var groupsResult = Result<IEnumerable<Group>>.Success(groups);
-        //    var rolesResult = Result<IEnumerable<Role>>.Success(roles);
+            var groupsResult = Result<IEnumerable<Group>>.Success(groups);
+            var rolesResult = Result<IEnumerable<Role>>.Success(roles);
 
-        //    _groupRepositoryMock
-        //        .Setup(repo => repo.GetAllAsync(It.IsAny<CancellationToken>()))
-        //        .ReturnsAsync(groupsResult);
+            _groupRepositoryMock
+                .Setup(repo => repo.GetAllAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(groupsResult);
 
-        //    _roleRepositoryMock
-        //        .Setup(repo => repo.GetRolesForClientAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
-        //        .ReturnsAsync(rolesResult);
+            _roleRepositoryMock
+                .Setup(repo => repo.GetRolesForClientAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(rolesResult);
 
-        //    _groupRolesRepositoryMock
-        //        .Setup(repo => repo.RemoveRoleFromGroupAsync(It.IsAny<Guid>(), It.IsAny<Guid>(), It.IsAny<Guid>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
-        //        .ReturnsAsync(groupsResult);
+            _groupRolesRepositoryMock
+                .Setup(repo => repo.RemoveRoleFromGroupAsync(It.IsAny<Guid>(), It.IsAny<Guid>(), It.IsAny<Guid>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(groupsResult);
 
-        //    // Act
-        //    var result = await _handler.Handle(command, cancellationToken);
+            // Act
+            var result = await _handler.Handle(command, cancellationToken);
 
-        //    // Assert
-        //    result
-        //        .IsSuccess
-        //        .Should()
-        //        .BeTrue();
+            // Assert
+            result
+                .IsSuccess
+                .Should()
+                .BeTrue();
 
-        //    _groupRepositoryMock.Verify(repo => repo.GetAllAsync(It.IsAny<CancellationToken>()), Times.Once);
-        //    _roleRepositoryMock.Verify(repo => repo.GetRolesForClientAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()), Times.Once);
+            _groupRepositoryMock.Verify(repo => repo.GetAllAsync(It.IsAny<CancellationToken>()), Times.Once);
+            _roleRepositoryMock.Verify(repo => repo.GetRolesForClientAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()), Times.Once);
 
-        //    _groupRolesRepositoryMock
-        //        .Verify(repo => repo.RemoveRoleFromGroupAsync(It.IsAny<Guid>(), It.IsAny<Guid>(), It.IsAny<Guid>(), It.IsAny<string>(), It.IsAny<CancellationToken>()));
+            _groupRolesRepositoryMock
+                .Verify(repo => repo.RemoveRoleFromGroupAsync(It.IsAny<Guid>(), It.IsAny<Guid>(), It.IsAny<Guid>(), It.IsAny<string>(), It.IsAny<CancellationToken>()));
 
-        //    _groupRepositoryMock.VerifyNoOtherCalls();
-        //    _roleRepositoryMock.VerifyNoOtherCalls();
-        //    _groupRolesRepositoryMock.VerifyNoOtherCalls();
-        //}
+            _groupRepositoryMock.VerifyNoOtherCalls();
+            _roleRepositoryMock.VerifyNoOtherCalls();
+            _groupRolesRepositoryMock.VerifyNoOtherCalls();
+        }
 
         [Fact]
         public async Task Given_RepositoryFails_When_RemoveRoleFromGroupIsCalled_Then_ShouldReturnFailure()
---
+++ tests/Feijuca.Auth.UnitTests/Queries/Permissions/GetRolesQueryHandlerTests.cs
@@ -0,0 +1,124 @@
+﻿using AutoFixture;
+using Feijuca.Auth.Application.Queries.Permissions;
+using Feijuca.Auth.Common.Errors;
+using Feijuca.Auth.Common.Models;
+using Feijuca.Auth.Domain.Entities;
+using Feijuca.Auth.Domain.Interfaces;
+using FluentAssertions;
+using Moq;
+
+namespace Feijuca.Auth.Api.UnitTests.Queries.Permissions
+{
+    public class GetRolesQueryHandlerTests
+    {
+        private readonly IFixture _fixture = new Fixture();
+        private readonly Mock<IClientRepository> _clientRepositoryMock = new();
+        private readonly Mock<IRoleRepository> _roleRepositoryMock = new();
+        private readonly GetRolesQueryHandler _handler;
+
+        public GetRolesQueryHandlerTests()
+        {
+            _handler = new GetRolesQueryHandler(_clientRepositoryMock.Object, _roleRepositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task Given_NoClientsExist_WhenGetClientsCalled_Then_ReturnsFailure()
+        {
+            // Arrange
+            var rolesQuery = _fixture.Create<GetRolesQuery>();
+            var cancellationToken = _fixture.Create<CancellationToken>();
+            var rolesResult = Result<IEnumerable<Client>>.Failure(RoleErrors.GetRoleErrors);
+
+            _clientRepositoryMock
+                .Setup(repo => repo.GetClientsAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(rolesResult);
+
+            // Act
+            var result = await _handler.Handle(rolesQuery, cancellationToken);
+
+            // Assert
+            result
+                .Error
+                .Should()
+                .Be(RoleErrors.GetRoleErrors);
+
+            _clientRepositoryMock.Verify(repo => repo.GetClientsAsync(It.IsAny<CancellationToken>()), Times.Once());
+            _clientRepositoryMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task Given_ClientsExistButNoRoles_WhenGetRolesForClientCalled_Then_ReturnsEmptyResponse()
+        {
+            // Arrange
+            var rolesQuery = _fixture.Create<GetRolesQuery>();
+            var cancellationToken = _fixture.Create<CancellationToken>();
+            var clients = _fixture.CreateMany<Client>();
+            var clientsResult = Result<IEnumerable<Client>>.Success(clients);
+            var rolesResult = Result<IEnumerable<Role>>.Failure(RoleErrors.GetRoleErrors);
+
+            _clientRepositoryMock
+                .Setup(repo => repo.GetClientsAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(clientsResult);
+
+            _roleRepositoryMock
+                .Setup(repo => repo.GetRolesForClientAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(rolesResult);
+
+            // Act
+            var result = await _handler.Handle(rolesQuery, cancellationToken);
+
+            // Assert
+            result
+                .IsSuccess
+                .Should()
+                .BeTrue();
+
+            result
+                .Response
+                .Should()
+                .BeEmpty(); 
+
+            _clientRepositoryMock.Verify(repo => repo.GetClientsAsync(It.IsAny<CancellationToken>()), Times.Once);
+            _clientRepositoryMock.VerifyNoOtherCalls();
+
+            _roleRepositoryMock.Verify(repo => repo.GetRolesForClientAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()), Times.Exactly(clients.Count()));
+            _roleRepositoryMock.VerifyNoOtherCalls();
+        }
+
+
+        [Fact]
+        public async Task Given_ClientsAndRolesExist_WhenGetRolesForClientCalled_Then_ReturnsSuccess()
+        {
+            // Arrange
+            var rolesQuery = _fixture.Create<GetRolesQuery>();
+            var cancellationToken = _fixture.Create<CancellationToken>();
+            var clients = _fixture.CreateMany<Client>();
+            var clientsResult = Result<IEnumerable<Client>>.Success(clients);
+            var roles = _fixture.CreateMany<Role>();
+            var rolesResult = Result<IEnumerable<Role>>.Success(roles);
+
+            _clientRepositoryMock
+                .Setup(repo => repo.GetClientsAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(clientsResult);
+
+            _roleRepositoryMock
+                .Setup(repo => repo.GetRolesForClientAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(rolesResult);
+
+            // Act
+            var result = await _handler.Handle(rolesQuery, cancellationToken);
+
+            // Assert
+            result
+                .IsSuccess
+                .Should()
+                .BeTrue();
+
+            _clientRepositoryMock.Verify(repo => repo.GetClientsAsync(It.IsAny<CancellationToken>()), Times.Once());
+            _clientRepositoryMock.VerifyNoOtherCalls();
+
+            _roleRepositoryMock.Verify(repo => repo.GetRolesForClientAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()), Times.Exactly(roles.Count()));
+            _roleRepositoryMock.VerifyNoOtherCalls();
+        }
+    }
+}
