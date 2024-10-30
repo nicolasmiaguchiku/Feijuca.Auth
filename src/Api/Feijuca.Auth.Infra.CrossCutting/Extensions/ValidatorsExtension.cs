aae8e70e4c9cd3a078a818b566b2ed0e0286af0f
Weslley Souza <weslleyjunio3032@gmail.com>
2024-10-29 22:04:15 -0300

    feat: Implemented the validator configuration and also the Login validation.

---
+++ src/Api/Feijuca.Auth.Infra.CrossCutting/Extensions/ValidatorsExtension.cs
@@ -0,0 +1,19 @@
+﻿using Feijuca.Auth.Application.Requests.Auth;
+using Feijuca.Auth.Application.Validators;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Feijuca.Auth.Infra.CrossCutting.Extensions
+{
+    public static class ValidatorsExtension
+    {
+        public static IServiceCollection AddValidators(this IServiceCollection services)
+        {
+            services.AddFluentValidationAutoValidation(x => x.DisableDataAnnotationsValidation = true);
+            services.AddScoped<IValidator<LoginUserRequest>, LoginUserRequestValidator>();
+
+            return services;
+        }
+    }
+}
