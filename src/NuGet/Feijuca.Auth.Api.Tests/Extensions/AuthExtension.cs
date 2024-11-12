cfd49ad2b6535e3cd0f2b4c3f07003d2da79aef0
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2024-11-12 17:17:52 -0300

    FEAT: Updating to .net9

--- src/NuGet/Feijuca.Auth.Api.Tests/Extensions/AuthExtension.cs
+++ src/NuGet/Feijuca.Auth.Api.Tests/Extensions/AuthExtension.cs
@@ -2,20 +2,20 @@
 using Feijuca.Auth.Models;
 using System.IdentityModel.Tokens.Jwt;
 
-namespace Feijuca.Auth.Api.Tests.Extensions
+namespace Feijuca.Auth.Api.Tests.Extensions;
+
+public static class AuthExtension
 {
-    public static class AuthExtension
+    public static IServiceCollection AddApiAuthentication(this IServiceCollection services, ServerSettings authSettings)
     {
-        public static IServiceCollection AddApiAuthentication(this IServiceCollection services, ServerSettings authSettings)
-        {
-            var xx = authSettings;
-            var client = new Client { ClientId = xx.Url };
+        var xx = authSettings;
+        var client = new Client { ClientId = xx.Url };
 
-            services.AddHttpContextAccessor();
-            services.AddSingleton<JwtSecurityTokenHandler>();
-            services.AddKeyCloakAuth(client, null!, null!, null!);
+        services.AddHttpContextAccessor();
+        services.AddSingleton<JwtSecurityTokenHandler>();
+        services.AddKeyCloakAuth(client, null!, null!, null!);
 
-            return services;
-        }
+        return services;
     }
 }
+
