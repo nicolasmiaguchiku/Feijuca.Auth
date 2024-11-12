962cb4471b05275f7fa076ff82be12809661a84a
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2024-11-12 17:32:51 -0300

    FEAT: Updating pipeline

--- src/NuGet/Feijuca.Auth/Models/Realm.cs
+++ src/NuGet/Feijuca.Auth/Models/Realm.cs
@@ -1,9 +1,8 @@
-﻿namespace Feijuca.Auth.Models
+﻿namespace Feijuca.Auth.Models;
+
+public class Realm
 {
-    public class Realm
-    {
-        public required string Name { get; init; }
-        public required string Audience { get; init; }
-        public required string Issuer { get; init; }
-    }
+    public required string Name { get; init; }
+    public required string Audience { get; init; }
+    public required string Issuer { get; init; }
 }
