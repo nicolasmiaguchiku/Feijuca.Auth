abad1f514a3772cf7b4214fbcf9ab16ffa4db95f
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2024-12-21 12:30:18 -0300

    FEAT: Adding new properties

---
+++ src/NuGet/Feijuca.Auth/Models/FeijucaAuthClient.cs
@@ -0,0 +1,8 @@
+﻿namespace Feijuca.Auth.Models
+{
+    public class FeijucaAuthClient
+    {
+        public required string FeijucaAuthClientId { get; init; }
+        public required string FeijucaAuthClientSecret { get; init; }
+    }
+}
--- src/NuGet/Feijuca.Auth/Models/Realm.cs
+++ src/NuGet/Feijuca.Auth/Models/Realm.cs
@@ -1,11 +1,12 @@
-﻿namespace Feijuca.Auth.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace Feijuca.Auth.Models;
 
 public class Realm
 {
-    public required string FeijucaAuthClientId { get; init; }
-    public required string FeijucaAuthClientSecret { get; init; }
+    [JsonIgnore]
+    public FeijucaAuthClient? FeijucaAuthClient { get; init; }
     public required string Name { get; init; }
     public required string Audience { get; init; }
     public required string Issuer { get; init; }
-    public bool DefaultSwaggerTokenGeneration { get; init; }
 }
