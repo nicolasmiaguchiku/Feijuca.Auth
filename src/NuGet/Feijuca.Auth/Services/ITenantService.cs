0cbb34015b984e8d0c5a59062d5e0b97ba635d21
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2025-04-06 08:25:18 -0300

    FEAT: Adding new changes

--- src/NuGet/Feijuca.Auth/Services/ITenantService.cs
+++ src/NuGet/Feijuca.Auth/Services/ITenantService.cs
@@ -1,16 +1,15 @@
 ﻿using Feijuca.Auth.Models;
 
-namespace Feijuca.Auth.Services
-{
-    public interface ITenantService
-    {
-        Tenant Tenant { get; }
-        User User { get; }
+namespace Feijuca.Auth.Services;
 
-        string GetInfoFromToken(string infoName);
-        Tenant GetTenantFromToken();
-        User GetUserFromToken();
-        void SetTenant(Tenant tenant);
-        void SetUser(User user);
-    }
+public interface ITenantService
+{
+    Tenant Tenant { get; }
+    User User { get; }
+
+    string GetInfoFromToken(string infoName);
+    Tenant GetTenantFromToken();
+    User GetUserFromToken();
+    void SetTenant(Tenant tenant);
+    void SetUser(User user);
 }
--- src/NuGet/Feijuca.Auth/Services/TenantService.cs
+++ src/NuGet/Feijuca.Auth/Services/TenantService.cs
@@ -2,78 +2,77 @@
 using Microsoft.AspNetCore.Http;
 using System.IdentityModel.Tokens.Jwt;
 
-namespace Feijuca.Auth.Services
+namespace Feijuca.Auth.Services;
+
+public class TenantService(IHttpContextAccessor httpContextAccessor, JwtSecurityTokenHandler jwtSecurityTokenHandler) : ITenantService
 {
-    public class TenantService(IHttpContextAccessor httpContextAccessor, JwtSecurityTokenHandler jwtSecurityTokenHandler) : ITenantService
+
+    private Tenant _tenant = null!;
+    private User _userId = null!;
+
+    public Tenant Tenant => _tenant;
+    public User User => _userId;
+
+    public Tenant GetTenantFromToken()
     {
-
-        private Tenant _tenant = null!;
-        private User _userId = null!;
-
-        public Tenant Tenant => _tenant;
-        public User User => _userId;
-
-        public Tenant GetTenantFromToken()
+        string jwtToken = GetToken();
+        if (!string.IsNullOrEmpty(jwtToken))
         {
-            string jwtToken = GetToken();
-            if (!string.IsNullOrEmpty(jwtToken))
-            {
-                var tokenInfos = jwtSecurityTokenHandler.ReadJwtToken(jwtToken);
-                var tenantClaim = tokenInfos.Claims.FirstOrDefault(c => c.Type == "tenant")?.Value!;
-                return new Tenant(tenantClaim, tenantClaim);
-            }
-
-            return new Tenant(string.Empty, string.Empty);
+            var tokenInfos = jwtSecurityTokenHandler.ReadJwtToken(jwtToken);
+            var tenantClaim = tokenInfos.Claims.FirstOrDefault(c => c.Type == "tenant")?.Value!;
+            return new Tenant(tenantClaim, tenantClaim);
         }
 
-        public string GetInfoFromToken(string infoName)
-        {
-            string jwtToken = GetToken();
-            if (!string.IsNullOrEmpty(jwtToken))
-            {
-                var tokenInfos = jwtSecurityTokenHandler.ReadJwtToken(jwtToken);
-                var userClaim = tokenInfos.Claims.FirstOrDefault(c => c.Type == infoName)?.Value!;
-                return userClaim;
-            }
+        return new Tenant(string.Empty, string.Empty);
+    }
 
+    public string GetInfoFromToken(string infoName)
+    {
+        string jwtToken = GetToken();
+        if (!string.IsNullOrEmpty(jwtToken))
+        {
+            var tokenInfos = jwtSecurityTokenHandler.ReadJwtToken(jwtToken);
+            var userClaim = tokenInfos.Claims.FirstOrDefault(c => c.Type == infoName)?.Value!;
+            return userClaim;
+        }
+
+        return string.Empty;
+    }
+
+    public User GetUserFromToken()
+    {
+        string jwtToken = GetToken();
+        if (!string.IsNullOrEmpty(jwtToken))
+        {
+            var tokenInfos = jwtSecurityTokenHandler.ReadJwtToken(jwtToken);
+            var userId = tokenInfos.Claims.FirstOrDefault(c => c.Type == "sub")?.Value!;
+            var userName = tokenInfos.Claims.FirstOrDefault(c => c.Type == "preferred_username")?.Value!;
+            return new User(Guid.Parse(userId), userName);
+        }
+
+        return new User(Guid.Empty, string.Empty);
+    }
+
+    private string GetToken()
+    {
+        var authorizationHeader = httpContextAccessor.HttpContext?.Request.Headers.Authorization.FirstOrDefault();
+
+        if (string.IsNullOrEmpty(authorizationHeader) || !authorizationHeader.StartsWith("Bearer "))
+        {
             return string.Empty;
         }
 
-        public User GetUserFromToken()
-        {
-            string jwtToken = GetToken();
-            if (!string.IsNullOrEmpty(jwtToken))
-            {
-                var tokenInfos = jwtSecurityTokenHandler.ReadJwtToken(jwtToken);
-                var userId = tokenInfos.Claims.FirstOrDefault(c => c.Type == "sub")?.Value!;
-                var userName = tokenInfos.Claims.FirstOrDefault(c => c.Type == "preferred_username")?.Value!;
-                return new User(Guid.Parse(userId), userName);
-            }
-
-            return new User(Guid.Empty, string.Empty);
-        }
-
-        private string GetToken()
-        {
-            var authorizationHeader = httpContextAccessor.HttpContext?.Request.Headers.Authorization.FirstOrDefault();
-
-            if (string.IsNullOrEmpty(authorizationHeader) || !authorizationHeader.StartsWith("Bearer "))
-            {
-                return string.Empty;
-            }
-
-            return authorizationHeader["Bearer ".Length..];
-        }
+        return authorizationHeader["Bearer ".Length..];
+    }
 
 
-        public void SetTenant(Tenant tenant)
-        {
-            _tenant = tenant;
-        }
+    public void SetTenant(Tenant tenant)
+    {
+        _tenant = tenant;
+    }
 
-        public void SetUser(User user)
-        {
-            _userId = user;
-        }
+    public void SetUser(User user)
+    {
+        _userId = user;
     }
 }
