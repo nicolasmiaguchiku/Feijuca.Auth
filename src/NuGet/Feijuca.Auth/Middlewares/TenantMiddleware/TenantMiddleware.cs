b8583d3ef15858894f7c0186aebe541ec7fe71b2
Richard Garcia <code.richardy@gmail.com>
2025-04-14 12:15:19 -0300

    this refactoring moves the `TenantMiddleware` class to its own folder and adds configurable options.  It is now possible to configure additional URLs that do not require JWT authentication, in addition to the standard URLs (“scalar”, “openapi”, “events”, “favicon.ico”). The error handling logic has also been slightly improved for better code clarity and readability.

--- src/NuGet/Feijuca.Auth/Middlewares/TenantMiddleware/TenantMiddleware.cs
+++ src/NuGet/Feijuca.Auth/Middlewares/TenantMiddleware/TenantMiddleware.cs
@@ -3,13 +3,18 @@ using Microsoft.AspNetCore.Http;
 
 namespace Feijuca.Auth.Middlewares
 {
-    public class TenantMiddleware(RequestDelegate next)
+    public class TenantMiddleware(RequestDelegate next, TenantMiddlewareOptions options)
     {
+        private static readonly List<string> _defaultUrls = ["scalar", "openapi", "events", "favicon.ico"];
+        private readonly List<string> _availableUrls = _defaultUrls
+            .Union(options.AvailableUrls ?? Enumerable.Empty<string>(), StringComparer.OrdinalIgnoreCase)
+            .ToList();
+
+
         public async Task InvokeAsync(HttpContext context, ITenantService tenantService)
         {
             var path = context.Request.Path.Value!;
-            var availableStartingUrls = new List<string> { "scalar", "openapi", "events", "favicon.ico" };
-            if (availableStartingUrls.Exists(path.Contains))
+            if (_availableUrls.Exists(path.Contains))
             {
                 await next(context);
                 return;
@@ -22,7 +27,9 @@ namespace Feijuca.Auth.Middlewares
             {
                 context.Response.StatusCode = StatusCodes.Status400BadRequest;
                 context.Response.ContentType = "application/json";
+
                 var response = new { error = "Jwt token authorization header is required." };
+
                 await context.Response.WriteAsJsonAsync(response);
                 return;
             }
