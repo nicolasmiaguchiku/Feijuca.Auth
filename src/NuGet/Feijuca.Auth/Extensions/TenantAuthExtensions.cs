0b123be3b2196af71f9c01cbbfdfd1684495ec20
Felipe Mattioli <felipe-mattioli98@hotmail.com>
2025-05-16 15:41:51 -0300

    FIX: Reordering file

--- src/NuGet/Feijuca.Auth/Extensions/TenantAuthExtensions.cs
+++ src/NuGet/Feijuca.Auth/Extensions/TenantAuthExtensions.cs
@@ -8,221 +8,220 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 
-namespace Feijuca.Auth.Extensions
+namespace Feijuca.Auth.Extensions;
+
+public static class TenantAuthExtensions
 {
-    public static class TenantAuthExtensions
+    public static IServiceCollection AddApiAuthentication(this IServiceCollection services, FeijucaAuthSettings settings)
     {
-        public static IServiceCollection AddApiAuthentication(this IServiceCollection services, FeijucaAuthSettings settings)
-        {
-            services.AddHttpContextAccessor();
-            services.AddKeyCloakAuth(settings.Client, settings.ServerSettings, settings.Realms);
+        services.AddHttpContextAccessor();
+        services.AddKeyCloakAuth(settings.Client, settings.ServerSettings, settings.Realms);
 
-            return services;
-        }
+        return services;
+    }
 
-        public static IServiceCollection AddKeyCloakAuth(this IServiceCollection services,
-            Client client,
-            ServerSettings serverSettings,
-            IEnumerable<Realm> realms,
-            IEnumerable<Policy>? policies = null)
-        {
-            services
-                .AddSingleton<JwtSecurityTokenHandler>()
-                .AddScoped<ITenantService, TenantService>()
-                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddKeycloakWebApi(
-                    options =>
+    public static IServiceCollection AddKeyCloakAuth(this IServiceCollection services,
+        Client client,
+        ServerSettings serverSettings,
+        IEnumerable<Realm> realms,
+        IEnumerable<Policy>? policies = null)
+    {
+        services
+            .AddSingleton<JwtSecurityTokenHandler>()
+            .AddScoped<ITenantService, TenantService>()
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddKeycloakWebApi(
+                options =>
+                {
+                    options.Resource = client.ClientId;
+                    options.AuthServerUrl = serverSettings.Url;
+                    options.VerifyTokenAudience = true;
+                },
+                options =>
+                {
+                    options.Events = new JwtBearerEvents
                     {
-                        options.Resource = client.ClientId;
-                        options.AuthServerUrl = serverSettings.Url;
-                        options.VerifyTokenAudience = true;
-                    },
-                    options =>
-                    {
-                        options.Events = new JwtBearerEvents
-                        {
-                            OnMessageReceived = OnMessageReceived(realms),
-                            OnAuthenticationFailed = OnAuthenticationFailed,
-                            OnChallenge = OnChallenge
-                        };
-                    });
+                        OnMessageReceived = OnMessageReceived(realms),
+                        OnAuthenticationFailed = OnAuthenticationFailed,
+                        OnChallenge = OnChallenge
+                    };
+                });
 
-            ConfigureAuthorization(services, client, policies);
+        ConfigureAuthorization(services, client, policies);
 
-            return services;
-        }
+        return services;
+    }
 
-        private static Func<MessageReceivedContext, Task> OnMessageReceived(IEnumerable<Realm> realms)
+    private static Func<MessageReceivedContext, Task> OnMessageReceived(IEnumerable<Realm> realms)
+    {
+        return async context =>
         {
-            return async context =>
+            try
             {
-                try
+                var tokenJwt = context.Request.Headers.Authorization.FirstOrDefault();
+                if (IsTokenValid(context, tokenJwt).Equals(false))
                 {
-                    var tokenJwt = context.Request.Headers.Authorization.FirstOrDefault();
-                    if (IsTokenValid(context, tokenJwt).Equals(false))
-                    {
-                        return;
-                    }
-
-                    var token = tokenJwt!.Replace("Bearer ", "");
-                    var tokenInfos = new JwtSecurityTokenHandler().ReadJwtToken(token);
-
-                    if (IsTokenExpirationValid(context, tokenInfos).Equals(false))
-                    {
-                        return;
-                    }
-
-                    var tenantName = tokenInfos.Claims.FirstOrDefault(c => c.Type == "tenant")?.Value;
-                    var tenantRealm = realms.FirstOrDefault(realm => realm.Name == tenantName);
-
-                    if (ValidateRealm(context, tenantRealm).Equals(false))
-                    {
-                        return;
-                    }
-
-                    if (IsTokenValidAudience(context, tokenInfos).Equals(false))
-                    {
-                        return;
-                    }
-
-                    var tokenValidationParameters = await GetTokenValidationParameters(token);
-                    var claims = new JwtSecurityTokenHandler().ValidateToken(token, tokenValidationParameters, out var _);
-
-                    context.Principal = claims;
-                    context.Success();
+                    return;
                 }
-                catch (Exception e)
+
+                var token = tokenJwt!.Replace("Bearer ", "");
+                var tokenInfos = new JwtSecurityTokenHandler().ReadJwtToken(token);
+
+                if (IsTokenExpirationValid(context, tokenInfos).Equals(false))
                 {
-                    context.Response.StatusCode = 500;
-                    context.HttpContext.Items["AuthError"] = $"Authentication error: {e.Message}";
-                    await context.Response.WriteAsJsonAsync(new { error = e.Message });
-                    context.Fail(e.Message);
+                    return;
                 }
-            };
-        }
 
-        private static Task OnAuthenticationFailed(AuthenticationFailedContext context)
+                var tenantName = tokenInfos.Claims.FirstOrDefault(c => c.Type == "tenant")?.Value;
+                var tenantRealm = realms.FirstOrDefault(realm => realm.Name == tenantName);
+
+                if (ValidateRealm(context, tenantRealm).Equals(false))
+                {
+                    return;
+                }
+
+                if (IsTokenValidAudience(context, tokenInfos).Equals(false))
+                {
+                    return;
+                }
+
+                var tokenValidationParameters = await GetTokenValidationParameters(token);
+                var claims = new JwtSecurityTokenHandler().ValidateToken(token, tokenValidationParameters, out var _);
+
+                context.Principal = claims;
+                context.Success();
+            }
+            catch (Exception e)
+            {
+                context.Response.StatusCode = 500;
+                context.HttpContext.Items["AuthError"] = $"Authentication error: {e.Message}";
+                await context.Response.WriteAsJsonAsync(new { error = e.Message });
+                context.Fail(e.Message);
+            }
+        };
+    }
+
+    private static Task OnAuthenticationFailed(AuthenticationFailedContext context)
+    {
+        var errorMessage = context.HttpContext.Items["AuthError"] as string ?? "Authentication failed!";
+        var statusCode = context.HttpContext.Items["AuthStatusCode"] as int? ?? 401;
+        context.Response.StatusCode = statusCode;
+        context.Response.ContentType = "application/json";
+        return context.Response.WriteAsJsonAsync(new { error = errorMessage });
+    }
+
+    private static async Task OnChallenge(JwtBearerChallengeContext context)
+    {
+        if (!context.Response.HasStarted)
         {
             var errorMessage = context.HttpContext.Items["AuthError"] as string ?? "Authentication failed!";
             var statusCode = context.HttpContext.Items["AuthStatusCode"] as int? ?? 401;
             context.Response.StatusCode = statusCode;
             context.Response.ContentType = "application/json";
-            return context.Response.WriteAsJsonAsync(new { error = errorMessage });
+            await context.Response.WriteAsJsonAsync(new { Message = errorMessage });
         }
 
-        private static async Task OnChallenge(JwtBearerChallengeContext context)
-        {
-            if (!context.Response.HasStarted)
-            {
-                var errorMessage = context.HttpContext.Items["AuthError"] as string ?? "Authentication failed!";
-                var statusCode = context.HttpContext.Items["AuthStatusCode"] as int? ?? 401;
-                context.Response.StatusCode = statusCode;
-                context.Response.ContentType = "application/json";
-                await context.Response.WriteAsJsonAsync(new { Message = errorMessage });
-            }
+        context.HandleResponse();
+    }
 
-            context.HandleResponse();
+    private static bool IsTokenValid(MessageReceivedContext context, string? tokenJwt)
+    {
+        if (string.IsNullOrEmpty(tokenJwt))
+        {
+            context.HttpContext.Items["AuthError"] = "Invalid JWT token!";
+            context.HttpContext.Items["AuthStatusCode"] = 401;
+            return false;
         }
 
-        private static bool IsTokenValid(MessageReceivedContext context, string? tokenJwt)
+        return true;
+    }
+
+    private static bool IsTokenExpirationValid(MessageReceivedContext context, JwtSecurityToken tokenInfos)
+    {
+        var expirationClaim = tokenInfos.Claims.FirstOrDefault(c => c.Type == "exp")?.Value;
+        if (expirationClaim != null && long.TryParse(expirationClaim, out var expirationUnix))
         {
-            if (string.IsNullOrEmpty(tokenJwt))
+            var expirationDate = DateTimeOffset.FromUnixTimeSeconds(expirationUnix).UtcDateTime;
+            if (DateTime.UtcNow >= expirationDate)
             {
-                context.HttpContext.Items["AuthError"] = "Invalid JWT token!";
-                context.HttpContext.Items["AuthStatusCode"] = 401;
-                return false;
-            }
-
-            return true;
-        }
-
-        private static bool IsTokenExpirationValid(MessageReceivedContext context, JwtSecurityToken tokenInfos)
-        {
-            var expirationClaim = tokenInfos.Claims.FirstOrDefault(c => c.Type == "exp")?.Value;
-            if (expirationClaim != null && long.TryParse(expirationClaim, out var expirationUnix))
-            {
-                var expirationDate = DateTimeOffset.FromUnixTimeSeconds(expirationUnix).UtcDateTime;
-                if (DateTime.UtcNow >= expirationDate)
-                {
-                    context.Response.StatusCode = 401;
-                    context.HttpContext.Items["AuthError"] = "Token has expired.";
-                    context.Fail("Token expired");
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        private static bool ValidateRealm(MessageReceivedContext context, Realm? tenantRealm)
-        {
-            if (tenantRealm == null)
-            {
-                context.HttpContext.Items["AuthError"] = "Invalid realm config provided, please verify!";
-                context.HttpContext.Items["AuthStatusCode"] = 401;
-                context.Fail("Invalid realm config provided, please verify!");
-                return false;
-            }
-            return true;
-        }
-
-        private static bool IsTokenValidAudience(MessageReceivedContext context, JwtSecurityToken tokenInfos)
-        {
-            var audience = tokenInfos.Claims.FirstOrDefault(c => c.Type == "aud")?.Value;
-            if (audience != "feijuca-auth-api")
-            {
-                context.HttpContext.Items["AuthError"] = "Invalid audience, please configure an audience mapper on your realm!";
-                context.HttpContext.Items["AuthStatusCode"] = 403;
+                context.Response.StatusCode = 401;
+                context.HttpContext.Items["AuthError"] = "Token has expired.";
                 context.Fail("Token expired");
                 return false;
             }
-            return true;
         }
 
-        private static async Task<TokenValidationParameters> GetTokenValidationParameters(string jwtToken)
+        return true;
+    }
+
+    private static bool ValidateRealm(MessageReceivedContext context, Realm? tenantRealm)
+    {
+        if (tenantRealm == null)
         {
-            var handler = new JwtSecurityTokenHandler();
-            var token = handler.ReadJwtToken(jwtToken);
-
-            var issuer = token.Issuer;
-            var audience = token.Audiences.FirstOrDefault();
-
-            using var httpClient = new HttpClient();
-            var jwksUrl = $"{issuer}/protocol/openid-connect/certs";
-            var jwks = await httpClient.GetStringAsync(jwksUrl);
-            var jsonWebKeySet = new JsonWebKeySet(jwks);
-
-            return new TokenValidationParameters
-            {
-                ValidateIssuer = true,
-                ValidIssuer = issuer,
-                ValidateAudience = true,
-                ValidAudience = audience,
-                ValidateLifetime = true,
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKeys = jsonWebKeySet.Keys
-            };
+            context.HttpContext.Items["AuthError"] = "Invalid realm config provided, please verify!";
+            context.HttpContext.Items["AuthStatusCode"] = 401;
+            context.Fail("Invalid realm config provided, please verify!");
+            return false;
         }
+        return true;
+    }
 
-        private static void ConfigureAuthorization(IServiceCollection services, Client client, IEnumerable<Policy>? policySettings)
+    private static bool IsTokenValidAudience(MessageReceivedContext context, JwtSecurityToken tokenInfos)
+    {
+        var audience = tokenInfos.Claims.FirstOrDefault(c => c.Type == "aud")?.Value;
+        if (audience != "feijuca-auth-api")
         {
-            services
-               .AddAuthorization()
-               .AddKeycloakAuthorization();
+            context.HttpContext.Items["AuthError"] = "Invalid audience, please configure an audience mapper on your realm!";
+            context.HttpContext.Items["AuthStatusCode"] = 403;
+            context.Fail("Token expired");
+            return false;
+        }
+        return true;
+    }
 
-            foreach (var policy in policySettings ?? [])
+    private static async Task<TokenValidationParameters> GetTokenValidationParameters(string jwtToken)
+    {
+        var handler = new JwtSecurityTokenHandler();
+        var token = handler.ReadJwtToken(jwtToken);
+
+        var issuer = token.Issuer;
+        var audience = token.Audiences.FirstOrDefault();
+
+        using var httpClient = new HttpClient();
+        var jwksUrl = $"{issuer}/protocol/openid-connect/certs";
+        var jwks = await httpClient.GetStringAsync(jwksUrl);
+        var jsonWebKeySet = new JsonWebKeySet(jwks);
+
+        return new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = issuer,
+            ValidateAudience = true,
+            ValidAudience = audience,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKeys = jsonWebKeySet.Keys
+        };
+    }
+
+    private static void ConfigureAuthorization(IServiceCollection services, Client client, IEnumerable<Policy>? policySettings)
+    {
+        services
+           .AddAuthorization()
+           .AddKeycloakAuthorization();
+
+        foreach (var policy in policySettings ?? [])
+        {
+            if (!string.IsNullOrEmpty(policy.Name))
             {
-                if (!string.IsNullOrEmpty(policy.Name))
-                {
-                    services
-                        .AddAuthorizationBuilder()
-                        .AddPolicy(policy.Name, p =>
-                        {
-                            p.RequireResourceRolesForClient(
-                                client.ClientId,
-                                [.. policy.Roles!]);
-                        });
-                }
+                services
+                    .AddAuthorizationBuilder()
+                    .AddPolicy(policy.Name, p =>
+                    {
+                        p.RequireResourceRolesForClient(
+                            client.ClientId,
+                            [.. policy.Roles!]);
+                    });
             }
         }
     }
