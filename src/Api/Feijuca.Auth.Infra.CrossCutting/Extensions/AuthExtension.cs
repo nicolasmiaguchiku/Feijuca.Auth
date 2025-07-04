cc7fee83d318596552e523513d535ff5ef2d6406
Felipe Mattioli dos Santos <felipe.mattiolidossantos@msc.com>
2025-07-04 16:29:46 +0200

    FEAT: Forcing pipeline

--- src/Api/Feijuca.Auth.Infra.CrossCutting/Extensions/AuthExtension.cs
+++ src/Api/Feijuca.Auth.Infra.CrossCutting/Extensions/AuthExtension.cs
@@ -6,29 +6,28 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using System.IdentityModel.Tokens.Jwt;
 
-namespace Feijuca.Auth.Infra.CrossCutting.Extensions
+namespace Feijuca.Auth.Infra.CrossCutting.Extensions;
+
+public static class AuthExtension
 {
-    public static class AuthExtension
+    public static IServiceCollection AddApiAuthentication(this IServiceCollection services, out KeycloakSettings serverSettings)
     {
-        public static IServiceCollection AddApiAuthentication(this IServiceCollection services, out KeycloakSettings serverSettings)
+        var serviceProvider = services.BuildServiceProvider();
+        var KeycloakSettingsRepository = serviceProvider.GetRequiredService<IConfigRepository>();
+        serverSettings = KeycloakSettingsRepository.GetConfigAsync().GetAwaiter().GetResult();
+
+        services.AddHttpContextAccessor();
+
+        if (serverSettings is not null)
         {
-            var serviceProvider = services.BuildServiceProvider();
-            var KeycloakSettingsRepository = serviceProvider.GetRequiredService<IConfigRepository>();
-            serverSettings = KeycloakSettingsRepository.GetConfigAsync().GetAwaiter().GetResult();
-
-            services.AddHttpContextAccessor();
-
-            if (serverSettings is not null)
-            {
-                services.AddKeyCloakAuth(serverSettings.Realms ?? []);
-
-                return services;
-            }
-
-            services.TryAddScoped<ITenantService, TenantService>();
-            services.TryAddSingleton<JwtSecurityTokenHandler>();
+            services.AddKeyCloakAuth(serverSettings.Realms ?? []);
 
             return services;
         }
+
+        services.TryAddScoped<ITenantService, TenantService>();
+        services.TryAddSingleton<JwtSecurityTokenHandler>();
+
+        return services;
     }
 }
