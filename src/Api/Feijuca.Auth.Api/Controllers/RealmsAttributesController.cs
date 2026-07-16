40c5de7d6997bc75b17616ce634348b91ed85abc
Miguel Totti <migueltotti2005@gmail.com>
2026-07-16 17:22:46 -0300

    FEAT: add endpoint para deletar um atributo de um realm específico

--- src/Api/Feijuca.Auth.Api/Controllers/RealmsAttributesController.cs
+++ src/Api/Feijuca.Auth.Api/Controllers/RealmsAttributesController.cs
@@ -1,4 +1,5 @@
 ﻿using Feijuca.Auth.Application.Commands.RealmAttributes;
+using Feijuca.Auth.Application.Commands.UserAttributes;
 using Feijuca.Auth.Application.Requests.RealmAttributes;
 using Feijuca.Auth.Attributes;
 using LiteBus.Commands.Abstractions;
@@ -58,6 +59,33 @@ public class RealmsAttributesController(ICommandMediator commandMediator) : Cont
     public async Task<IActionResult> UpdateAttributes([FromBody] UpdateRealmAttributesRequest updateRealmAttributeRequest, CancellationToken cancellationToken)
     {
         var result = await commandMediator.SendAsync(new UpdateRealmAttributesCommand(updateRealmAttributeRequest), cancellationToken);
+
+        if (result.IsSuccess)
+        {
+            return NoContent();
+        }
+
+        return BadRequest(result.Error);
+    }
+
+    /// <summary>
+    /// Removes a existing attributes from a specified Keycloak realm.
+    /// </summary>
+    /// <param name="attributeKeys">The request object containing the necessary details to add attribute to the user.</param>
+    /// <param name="cancellationToken">A <see cref="T:System.Threading.CancellationToken"/> that can be used to signal cancellation for the operation.</param>
+    /// <returns>
+    /// A 204 No Content status code if the user is successfully created;
+    /// otherwise, a 400 Bad Request status code with an error message.
+    /// </returns>
+    [HttpDelete]
+    [EndpointDescription("This endpoint delete existing attributes related to the an existing realm.")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [RequiredRole("Feijuca.ApiWriter")]
+    public async Task<IActionResult> DeleteAttributes([FromBody] IEnumerable<string> attributeKeys, CancellationToken cancellationToken)
+    {
+        var result = await commandMediator.SendAsync(new DeleteRealmAttributesCommand(attributeKeys), cancellationToken);
 
         if (result.IsSuccess)
         {
---
+++ src/Api/Feijuca.Auth.Application/Commands/RealmAttributes/DeleteRealmAttributesCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Feijuca.Auth.Domain.Interfaces;
+using Feijuca.Auth.Providers;
+using LiteBus.Commands.Abstractions;
+using Mattioli.Configurations.Models;
+
+namespace Feijuca.Auth.Application.Commands.RealmAttributes;
+
+public class DeleteRealmAttributesCommandHandler(IRealmRepository realmRepository, ITenantProvider tenantProvider) : ICommandHandler<DeleteRealmAttributesCommand, Result>
+{
+    public async Task<Result> HandleAsync(DeleteRealmAttributesCommand command, CancellationToken cancellationToken = default)
+    {
+        var realm = await realmRepository.GetAsync(tenantProvider.Tenant.Name, cancellationToken);
+        if (realm.IsSuccess)
+        {
+            var realmAttributes = realm.Data.Attributes;
+            foreach (var key in command.DeleteRealmAttributeRequest)
+            {
+                realmAttributes.Remove(key);
+            }
+
+            realm.Data.Attributes = realmAttributes;
+
+            var result = await realmRepository.UpdateRealmAsync(tenantProvider.Tenant.Name, realm.Data, cancellationToken);
+
+            if (result.IsSuccess)
+            {
+                return Result.Success();
+            }
+        }
+
+        return Result.Failure(realm.Error);
+    }
+}
