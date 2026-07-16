0104c7455c4f371551b5afb0910206a27f68d915
Miguel Totti <migueltotti2005@gmail.com>
2026-07-16 16:09:08 -0300

    FEAT: add endpoint para incluir um novo chave-valor na propriedade Attributes do realm

---
+++ src/Api/Feijuca.Auth.Api/Controllers/RealmsAttributesController.cs
@@ -0,0 +1,42 @@
+﻿using Feijuca.Auth.Application.Commands.RealmAttributes;
+using Feijuca.Auth.Application.Requests.RealmAttributes;
+using Feijuca.Auth.Attributes;
+using LiteBus.Commands.Abstractions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Feijuca.Auth.Api.Controllers;
+
+[Route("api/v1/realms-attributes")]
+[ApiController]
+[Authorize]
+public class RealmsAttributesController(ICommandMediator commandMediator) : ControllerBase
+{
+    /// <summary>
+    /// Adds a new attribute to the specified Keycloak realm.
+    /// </summary>
+    /// <param name="addRealmAttributeRequest">The request object containing the necessary details to add attribute to the realm.</param>
+    /// <param name="cancellationToken">A <see cref="T:System.Threading.CancellationToken"/> that can be used to signal cancellation for the operation.</param>
+    /// <returns>
+    /// A 201 Created status code if the user is successfully created;
+    /// otherwise, a 400 Bad Request status code with an error message.
+    /// </returns>
+    [HttpPost]
+    [EndpointDescription("This endpoint add new attributes related to the an existing realm.")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [RequiredRole("Feijuca.ApiWriter")]
+    public async Task<IActionResult> AddAtribute([FromBody] AddRealmAttributesRequest addRealmAttributeRequest,
+        CancellationToken cancellationToken)
+    {
+        var result = await commandMediator.SendAsync(new AddRealmAttributesCommand(addRealmAttributeRequest), cancellationToken);
+
+        if (result.IsSuccess)
+        {
+            return Created("/", true);
+        }
+
+        return BadRequest(result.Error);
+    }
+}
--- src/Api/Feijuca.Auth.Domain/Interfaces/IRealmRepository.cs
+++ src/Api/Feijuca.Auth.Domain/Interfaces/IRealmRepository.cs
@@ -1,13 +1,16 @@
 ﻿using Feijuca.Auth.Domain.Entities;
+using Mattioli.Configurations.Models;
 
 namespace Feijuca.Auth.Domain.Interfaces
 {
     public interface IRealmRepository : IBaseRepository
     {
         Task<IEnumerable<RealmEntity>> GetAllAsync(CancellationToken cancellationToken);
+        Task<Result<RealmEntity>> GetAsync(string name, CancellationToken cancellationToken);
         Task<string> GetRealmConfigAsync(string name, CancellationToken cancellationToken);
         Task<bool> DeleteRealmAsync(string name, CancellationToken cancellationToken);
         Task<bool> CreateRealmAsync(RealmEntity realm, CancellationToken cancellationToken);
+        Task<Result> UpdateRealmAsync(string name, RealmEntity realm, CancellationToken cancellationToken);
         Task<bool> UpdateRealmUnmanagedAttributePolicyAsync(string realmName, CancellationToken cancellationToken);
         Task<bool> EnableDisableRealmAsync(string name, bool enable, CancellationToken cancellationToken);
     }
